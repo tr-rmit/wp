6af72d32ee28a4325d6a18e519d2ffc23a3818a7
Trevor Reynolds <trevor.reynolds@rmit.edu.au>
2025-03-26 21:16:41 +1100

    F4WA WK7 Close

---
+++ a2/index.php
@@ -0,0 +1,12 @@
+<?php 
+  $title = 'Home';
+  include('includes/header.inc'); 
+?>
+
+    <main id="myhome">
+      <button>Search <span class="material-symbols-outlined">home</span></button>
+      <img height="200" width="200" src="images/main.jpeg" alt="Game On! Grab your controller">
+      <h1>GAMESRUS</h1>
+      <h2>THE ULTIMATE GAMING DESTINATION</h2>
+    </main>
+<?php include('includes/footer.inc'); ?>
