e8e09865b091b6d372ea546a35279be5e70a5702
Trevor Reynolds <trevor.reynolds@rmit.edu.au>
2025-04-16 19:00:49 +1000

    F4WA WK10 Midpoint

---
+++ week10-F4WA/countries.php
@@ -0,0 +1,30 @@
+<?php 
+  $title = 'Countries';
+  include('includes/header.inc'); 
+?>
+<main>
+  <h1>Countries in a table</h1>
+
+  <table>
+  <tr>
+    <th>Name</th><th>Image</th><th>Caption</th>
+  </tr>
+<?php
+
+  $countries = mysqli_query($conn, "select * from country");
+  
+  while($row = mysqli_fetch_assoc($countries)) {
+    echo <<<"MINECRAFT"
+      <tr>
+        <td><a href="country.php?countryid={$row['countryid']}">{$row['countryname']}</a></td>
+        <td><img width="100" src="images/{$row['image']}" alt="{$row['caption']}"></td>
+        <td>{$row['caption']}</td>
+      </tr>
+
+MINECRAFT;      
+  }
+
+?>  
+</table>
+</main>
+<?php include('includes/footer.inc'); ?>
---
+++ week10-F4WA/country.php
@@ -0,0 +1,41 @@
+<?php 
+  if(!isset($_GET['countryid'])) {
+    header("Location: gallery.php");
+    exit();
+  }
+  $id = htmlentities($_GET['countryid']);
+  $title = 'Country';
+  include('includes/header.inc'); 
+?>
+<main>
+  <h1>Country Detail</h1>
+
+<?php
+
+  $sql = "select * from country where countryid=?";
+  $stmt = $conn->prepare($sql);
+  if (!$stmt) {
+    exit("prepare failed: " . $conn->error);
+  }
+  $stmt->bind_param("i", $id);
+  $stmt->execute();
+  $rows = $stmt->get_result();
+  if ($rows->num_rows > 0) {
+    foreach($rows as $row) { 
+      echo <<<"CDATA"
+      <div class="gallery-item">
+        <h3>{$row['countryname']}</h3>
+        <img src="images/{$row['image']}" alt="{$row['caption']}">
+        <p>{$row['description']}</p>
+      </div>
+
+CDATA;
+    }    
+  } else {
+    echo "<p>Sorry, no country found with that id.</p>";
+  }
+
+?>
+
+</main>
+<?php include('includes/footer.inc'); ?>
