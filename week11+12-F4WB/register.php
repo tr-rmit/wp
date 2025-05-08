49401ef31926a7f43d8d8b9fb5ba7075e2aa62ac
Trevor Reynolds <trevor.reynolds@rmit.edu.au>
2025-05-08 18:46:30 +1000

    F4WB Week 12 Midpoint

---
+++ week11+12-F4WB/register.php
@@ -0,0 +1,16 @@
+<?php 
+  $title = 'Register';
+  include('includes/header.inc'); 
+?>
+
+<main>
+  <h1>Register</h1>  
+  <form action="register_process.php" method="post">
+    <p><label>Name<sup>*</sup> <input name="username" type="text" placeholder="name" required></label></p>
+    <p><label>Pass<sup>*</sup> <input name="password" type="password" placeholder="password" required></label></p>
+    <p><button>Register</button></p>
+  </form>
+
+</main>
+
+<?php include('includes/footer.inc'); ?>
