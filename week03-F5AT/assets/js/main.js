c3018ae18a91863b53c57f11fb441121796e0e0b
tr-rmit <trevor.reynolds@rmit.edu.au>
2025-07-29 14:24:27 +1000

    F5AT WK3 Close

---
+++ week03-F5AT/assets/js/main.js
@@ -0,0 +1,45 @@
+// When the page loads
+document.addEventListener('DOMContentLoaded', function () {
+    
+    const galleryImages = document.querySelectorAll('.gallery-img');
+    const modalImage = document.getElementById('modalImage');
+
+    /* Code below is a compact form of a standard for loop
+    for(let i=0; i<galleryImages.length; i++) {
+        galleryImages[i]
+    }*/
+    // For each small gallery image
+    galleryImages.forEach(img => {
+        img.addEventListener('click', function () {
+            // Update the modal image to match the clicked image
+            modalImage.src = this.src;
+            modalImage.alt = this.alt;
+        });
+    });
+    // Show the modal when an image is clicked
+
+
+
+    // Client side form validation
+    if (document.getElementById('uploadForm')) {
+        document.getElementById('uploadForm').addEventListener('submit', function (event) {
+            const publishedInput = document.getElementById('published');
+            const errorMessage = document.getElementById('error-message');
+
+            // Hide any previous error message
+            errorMessage.classList.add('d-none');
+            errorMessage.textContent = '';
+
+            const currentYear = new Date().getFullYear();
+            const publishedYear = parseInt(publishedInput.value, 10);
+
+
+            if (isNaN(publishedYear) || publishedYear < 1900 || publishedYear > currentYear) {
+                errorMessage.textContent = "Published year must be between 1900 and " + currentYear;
+                errorMessage.classList.remove('d-none');
+                event.preventDefault();
+                return;
+            }
+        });
+    }
+});
