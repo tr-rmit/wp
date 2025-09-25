95ebff6261168d11b0f91adad9f2d000f1e8519a
tr-rmit <trevor.reynolds@rmit.edu.au>
2025-09-25 12:05:27 +1000

    F5AD WK10 Close

---
+++ week10-F5AD/add.php
@@ -0,0 +1,25 @@
+<?php
+include('includes/tools.inc');
+include('includes/db_connect.inc');
+$title = 'Add Country';
+include('includes/header.inc');
+?>
+  <main class="container">
+    <h2><?= $title ?></h2>
+    <form method="post" action="insert.php" enctype="multipart/form-data">
+    <div class="form-group">
+      <label>Country Name:</label>
+      <input type="text" name="countryname" class="form-control">
+      <label>Description:</label>
+      <textarea cols="50" rows="5" name="description" class="form-control"></textarea>
+      <label>Select an Image:</label>
+      <input type="file" name="image" class="form-control">
+      <label>Image Caption:</label>
+      <input type="text" name="caption" class="form-control">
+    </div>
+    <input type="submit" name="submit" value="Insert" class="btn btn-primary">
+</form>
+  </main>
+<?php
+include('includes/footer.inc');
+?>
---
+++ week10-F5AD/insert.php
@@ -0,0 +1,67 @@
+<?php
+include('includes/tools.inc');
+include('includes/db_connect.inc');
+$title = 'Insert';
+include('includes/header.inc');
+?>
+  <main class="container">
+    <h2><?= $title ?></h2>
+<?php
+// Step 1: Are we getting any POST and FILES data eg from add.php?
+  //preshow($_POST);
+  //preshow($_FILES);
+  //sleep(3);
+  
+  foreach ($_POST as $name => $value) {
+    $$name = trim($value);
+  }
+
+  $image = trim($_FILES['image']['name']);
+  $temp = $_FILES['image']['tmp_name'];
+  $error = $_FILES['image']['error'];
+  $type = $_FILES['image']['type'];
+  $size = $_FILES['image']['size'];
+
+  if(empty($image) || $error !=0 || $type != "image/jpg" || $size > 1000000) {
+    exit("This image is not suitable, no record inserted, no image uploaded");
+  }
+
+  $sql = "INSERT INTO country(countryname,description,image,caption) VALUES (?,?,?,?)"; //  '); drop tables; <-- this won't work
+
+  $stmt = $conn->prepare($sql);
+  if (!$stmt) {
+    exit("An error occurred when preparing the statement on the database.");
+  }
+
+  $stmt->bind_param("ssss", $countryname, $description, $image, $caption);
+  $stmt->execute();
+
+  if ($stmt->affected_rows > 0) {
+    echo '<div class="alert alert-success alert-dismissible">';
+    echo '<button type="button" class="btn-close" data-bs-dismiss="alert"></button>';
+    echo "<p>New record successfully inserted into the database</p>";
+    echo '</div>';
+    if (move_uploaded_file($temp, '../images/userimages/' . $image)) {
+      echo '<div class="alert alert-success alert-dismissible">';
+      echo '<button type="button" class="btn-close" data-bs-dismiss="alert"></button>';
+      echo "<p>Image moved to userimages folder</p>";
+      echo '</div>';
+    } else {
+      echo '<div class="alert alert-danger alert-dismissible">';
+      echo '<button type="button" class="btn-close" data-bs-dismiss="alert"></button>';
+      echo "<p>Image not moved to userimages folder</p>";
+      echo '</div>';
+    }
+  } else {
+    echo '<div class="alert alert-danger alert-dismissible">';
+    echo '<button type="button" class="btn-close" data-bs-dismiss="alert"></button>';
+    echo "<p>Record not inserted into the database</p>";
+    echo '</div>';
+  }
+
+?>    
+  </main>
+
+<?php
+include('includes/footer.inc');
+?>
